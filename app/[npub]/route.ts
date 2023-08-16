b79e50060923b957e4840be480ae35cda4ebcb45
Asai Toshiya <to.asai.60@gmail.com>
2023-08-17 02:29:38 +0900

    JavaScript を TypeScript に変換する
    Closes #5

--- app/[npub]/route.ts
+++ app/[npub]/route.ts
@@ -1,4 +1,5 @@
-import { SimplePool, nip19 } from "nostr-tools";
+import { SimplePool, nip19, Event, Kind } from "nostr-tools";
+import { NextRequest } from "next/server";
 
 const relays = [
   "wss://nos.lol",
@@ -10,7 +11,11 @@ const relays = [
   "wss://relay.nostr.bg",
 ];
 
-const fetchOldestPost = async (relays, author, until) => {
+const fetchOldestPost = async (
+  relays: string[],
+  author: string,
+  until: number
+) => {
   const pool = new SimplePool();
   const posts = (
     await pool.list(relays, [
@@ -23,28 +28,31 @@ const fetchOldestPost = async (relays, author, until) => {
   ).sort((a, b) => a.created_at - b.created_at);
   pool.close(relays);
   const olderPost = posts[0];
-  const oldestPost =
+  const oldestPost: Event<Kind.Text> =
     olderPost &&
     (await fetchOldestPost(relays, author, olderPost.created_at - 1));
   return oldestPost || olderPost;
 };
 
-export async function GET(request, { params }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { npub: string } }
+) {
   const npub = params.npub;
-  const hex = nip19.decode(npub).data;
+  const hex = nip19.decode(npub).data as string;
   const oldestPost = await fetchOldestPost(
     relays,
     hex,
     Math.floor(Date.now() / 1000)
   );
   const locale = request.nextUrl.searchParams.get("l");
-  const offset = parseInt(request.nextUrl.searchParams.get("t"));
+  const offset = parseInt(request.nextUrl.searchParams.get("t")!);
   const offsetSeconds = (offset ? offset : 0) * 60 * 60;
   return new Response(
     locale
       ? new Date((oldestPost.created_at + offsetSeconds) * 1000).toLocaleString(
           locale
         )
-      : oldestPost.created_at
+      : oldestPost.created_at.toString()
   );
 }
